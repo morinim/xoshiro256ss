9c66eed4983fb6ef3224512f2e8f5746c0071fcf
Manlio Morini <morinim@gmail.com>
2018-07-26 11:04:46 +0200

    Added the new xoshiro256** algorithm

--- example.cc
+++ example.cc
@@ -1,9 +1,30 @@
 #include <iomanip>
 #include <map>
 
-#include "xoroshiro128p.h"
+#include "xoshiro256ss.h"
 
-int main()
+void test_xoshiro256ss()
+{
+  // XOSHIRO256**
+  vigna::xoshiro256ss gen;
+
+  std::random_device dev;
+  gen.seed(dev());
+
+  std::uniform_int_distribution<> dist(0, 10);
+
+  std::map<unsigned, unsigned> hist;
+
+  for(unsigned n(0); n < 1000000; ++n)
+    ++hist[dist(gen)];
+
+  std::cout << "XOSHIRO256**\n";
+  for(auto p : hist)
+    std::cout << std::setw(3) << p.first << ' '
+              << std::string(p.second / 10000, '*') << '\n';
+}
+
+void test_xoroshiro128p()
 {
   vigna::xoroshiro128p gen;
 
@@ -17,7 +38,14 @@ int main()
   for(unsigned n(0); n < 1000000; ++n)
     ++hist[dist(gen)];
 
+  std::cout << "XOROSHIRO128**\n";
   for(auto p : hist)
     std::cout << std::setw(3) << p.first << ' '
               << std::string(p.second / 10000, '*') << '\n';
+}
+
+int main()
+{
+  test_xoshiro256ss();
+  test_xoroshiro128p();
 }
