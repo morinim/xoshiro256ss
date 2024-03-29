08ac3185258249fe3c25ab47a8ea3cf8d47f24f4
Manlio Morini <morinim@gmail.com>
2024-03-29 11:21:30 +0100

    Removed unused code

--- test.cc
+++ test.cc
@@ -3,46 +3,6 @@
 
 #include "xoshiro256ss.h"
 
-void test_xoshiro256ss()
-{
-  vigna::xoshiro256ss gen;
-
-  std::random_device dev;
-  gen.seed(dev());
-
-  std::uniform_int_distribution<> dist(0, 10);
-
-  std::map<unsigned, unsigned> hist;
-
-  for(unsigned n(0); n < 1000000; ++n)
-    ++hist[dist(gen)];
-
-  std::cout << "XOSHIRO256**\n";
-  for(auto p : hist)
-    std::cout << std::setw(3) << p.first << ' '
-              << std::string(p.second / 10000, '*') << '\n';
-}
-
-void test_xoroshiro128p()
-{
-  vigna::xoroshiro128p gen;
-
-  std::random_device dev;
-  gen.seed(dev());
-
-  std::uniform_int_distribution<> dist(0, 10);
-
-  std::map<unsigned, unsigned> hist;
-
-  for(unsigned n(0); n < 1000000; ++n)
-    ++hist[dist(gen)];
-
-  std::cout << "XOROSHIRO128+\n";
-  for(auto p : hist)
-    std::cout << std::setw(3) << p.first << ' '
-              << std::string(p.second / 10000, '*') << '\n';
-}
-
 int main()
 {
   const vigna::xoshiro256ss::result_type reference[] =
